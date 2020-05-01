e0d00a1aabe86890eee3c1235ee612de73beda95
CrisTo <toaquizacris@hotmail.com>
2020-05-01 16:28:22 -0500

    creating first component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<div>Hello from React Seed</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 module.hot.accept();
